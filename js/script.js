b61f8cebe4ee57c95403ec46ce4d33e02fd14459
cheisentrout <cheisentrout@gmail.com>
2021-02-08 14:19:51 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Create the object of "politician"
+var politician = {};
+
+//Create properties that attach to the object "politician"
+politician.name = "";
+politician.electionResults = null;
+politician.totalVotes = 0;
+
